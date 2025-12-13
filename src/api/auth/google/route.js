6cad369f93e774d8a899305bba507ebd776d188f
sergey <artemkruglikov370@gmail.com>
2025-12-14 01:55:31 +0700

    updated

---
+++ src/api/auth/google/route.js
@@ -0,0 +1,16 @@
+export default function googleRedirect(req, res) {
+    const params = new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      response_type: "code",
+      scope: "openid email profile",
+      access_type: "offline",
+      prompt: "consent"
+    });
+  
+    res.redirect(
+      `https://accounts.google.com/o/oauth2/v2/auth?${params}`
+    );
+  }
+  
+  
