cf204f22bc05fce36e49da2b75dcb612f7a945ec
sergey <artemkruglikov370@gmail.com>
2025-12-22 21:28:34 +0700

    fixed api

--- src/api/regProfile/route.js
+++ src/api/regProfile/route.js
@@ -2,35 +2,37 @@ import { Router } from "express";
 import { model } from "../../db.js";
 const routerReg = Router();
 
-
 routerReg.post("/", async (req, res) => {
   try {
-  const {username, email} = req.body 
-  const [checkEmail] = await model.query(
-    "SELECT * FROM profiles WHERE emails = ?",
-    [email]
-  );
-  if (Array.isArray(checkEmail) && checkEmail.length == 1) return res.json({
-    ok: false,
-    error: "email already exists"
-  });
+    const { username, email } = req.body;
 
-  const [addEmail] = await model.query(
-    "INSERT INTO profiles (username, emails) VALUES(?, ?)",
-    [username, email]
-  );
+    const [checkEmail] = await model.query(
+      "SELECT * FROM profiles WHERE emails = ?",
+      [email]
+    );
 
-  return res.status(200).json({
-    ok: true,
-    id: addEmail.id,
-    username,
-    email
-  })
+    if (Array.isArray(checkEmail) && checkEmail.length === 1) {
+      return res.status(409).json({
+        ok: false,
+        error: "email already exists",
+      });
+    }
 
-  } catch(e) {
-    console.log("something done wrong")
+    const [addEmail] = await model.query(
+      "INSERT INTO profiles (username, emails) VALUES(?, ?)",
+      [username, email]
+    );
+
+    return res.status(200).json({
+      ok: true,
+      id: addEmail.insertId,
+      username,
+      email,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ ok: false, error: "internal error" });
   }
-  
 });
 
-export default routerReg
+export default routerReg;
