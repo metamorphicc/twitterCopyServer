b90947bfa6c3d527fcde63318a0117408237b57c
sergey <artemkruglikov370@gmail.com>
2025-12-24 22:26:36 +0700

    new proxy

--- src/api/profiles/route.js
+++ src/api/profiles/route.js
@@ -4,42 +4,13 @@ import { Router } from "express";
 export const routerGetProfiles = Router();
 
 routerGetProfiles.post("/", async (req, res) => {
-  const { email, password } = req.body;
-  const [row] = await model.query(
-    "SELECT * FROM profiles WHERE emails = ? AND passwords = ?",
-    [email, password]
-  );
-
-  if (Array.isArray(row) && row.length != 0) {
-    const user = row[0];
-    res.status(200).json({
-      ok: true,
-      desc: "это первый иф",
-      id: user.id,
-      email: user.emails,
-      password: user.passwords,
-      user: row,
-    });
+  const {id} = req.body
+  const [row] = await model.query("SELECT tag, createdAt FROM profiles WHERE id = ?", [id])
+  console.log(row)
+  const result = row[0]
+  res.status(200).json({
+    ok: true,
+    result
   }
-
-  if (row.length == 0) {
-    const [rows] = await model.query(
-      "INSERT INTO profiles (emails, passwords) VALUES (?, ?)",
-      [email, password]
-    );
-
-    const insertedId = rows.id;
-
-    const [rows2] = await model.query("SELECT * FROM profiles WHERE id = ?", [
-      insertedId,
-    ]);
-
-    res.status(200).json({
-      ok: true,
-      desc: "это второй иф",
-      id: rows2.id,
-      email: rows2.emails,
-      password: rows2.passwords,
-    });
-  }
+  )
 });
