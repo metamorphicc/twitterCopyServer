746c0e4558805541d0e5f0b940915619f13cf7de
sergey <artemkruglikov370@gmail.com>
2025-12-25 00:24:05 +0700

    little fixes

--- src/api/getPosts/router.js
+++ src/api/getPosts/router.js
@@ -4,15 +4,13 @@ import { model } from "../../db.js";
 const router = Router();
 
 router.post(`/`, async (req, res) => {
-  const { id } = req.body;
+  const { ids } = req.body;
   try {
-    const [rows] = await model.query("SELECT * FROM posts WHERE profile_id = ? ", [id]);
-
-   
-
+    const [rows] = await model.query("SELECT * FROM posts WHERE profile_id = ? ", [ids]);
+    console.log(rows)
     res.status(200).json({
       ok: true,
-      id,
+      ids,
       rows,
     });
   } catch (e) {
