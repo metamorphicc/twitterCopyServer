431b4fda4cf486d8653e1ae20dcbe77c09158a93
sergey <artemkruglikov370@gmail.com>
2025-12-31 06:45:33 +0700

    updated

--- src/api/profiles/route.js
+++ src/api/profiles/route.js
@@ -5,9 +5,9 @@ export const routerGetProfiles = Router();
 
 routerGetProfiles.post("/", async (req, res) => {
   const {id} = req.body
-  const [row] = await model.query("SELECT tag, createdAt, following, followers FROM profiles WHERE id = ?", [id])
+  const [row] = await model.query("SELECT description, tag, createdAt, following, followers FROM profiles WHERE id = ?", [id])
   const result = row[0]
-  
+    console.log(result)
   res.status(200).json({
     ok: true,
     result
